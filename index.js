910a7a1fe6a97f41f4007b1d6f11c36feacabf6b
Prabhjot Singh <honey200509052nd@gmail.com>
2025-11-07 11:29:10 +0530

    testing name

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ message: 'App running successfully! , Tested Automated CI/CD' });
+  res.json({ message: 'App running successfully! , Hello Prabh' });
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
