ad561edc242848f83c15cb45022e55ccd758d304
Prabhjot Singh <honey200509052nd@gmail.com>
2025-11-07 15:54:30 +0530

    Fix: added dynamic PORT for Render deployment

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ app.get('/', (req, res) => {
   res.json({ message: 'App running successfully! , Hello Prabh' });
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(3000, () => console.log('Server running on port 3000'));
