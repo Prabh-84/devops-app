366f85e83d918f8262140b511c351e59e02c951e
Prabhjot Singh <honey200509052nd@gmail.com>
2025-11-07 16:48:16 +0530

    added ashwinder pal singh\

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-  res.json({ message: 'App running successfully! , Hello Ashwinder' });
+  res.json({ message: 'App running successfully! , Hello Ashwinder Pal Singh' });
 });
 
 // expose /metrics
