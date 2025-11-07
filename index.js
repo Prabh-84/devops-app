102cf761ea46a6f0342092aec875d9dbb8a23e58
Prabhjot Singh <honey200509052nd@gmail.com>
2025-11-07 15:56:58 +0530

    testing Hi ashwinder

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ message: 'App running successfully! , Hello Prabh' });
+  res.json({ message: 'App running successfully! , Hello Ashwinder' });
 });
 
 const PORT = process.env.PORT || 3000;
